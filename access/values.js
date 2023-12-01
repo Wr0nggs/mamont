b6e5406f6a902c4a49b5d04002b996893d91489f
Wr0nggs <152635522+Wr0nggs@users.noreply.github.com>
2023-12-01 17:50:10 +0200

    Add files via upload

---
+++ access/values.js
@@ -0,0 +1,11 @@
+// Активировать документы (true - включены, false - отключены)
+var isRightsEnabled = false; // Права
+var isCovidCertificateEnabled = false; // COVID-сертификат
+
+// Общее
+var birthdate = "19.02.2005"; // Дата рождения
+var fullname = "Терлецький Олександр Олегович" // ФИО
+var name = "Олександр" // Имя
+
+// Паспорт
+var passport_id = "739275497" // Номер паспорта
